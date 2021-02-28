1ba501f320f36ca88e5a852b67264bf40254ae0f
rusarwa <rusarwa@gmail.com>
2021-02-28 15:58:09 -0600

    Initial commit

---
+++ src/main/java/com/greeetingservice/demo/GreetingController.java
@@ -0,0 +1,17 @@
+package com.greeetingservice.demo;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class GreetingController {
+    @RequestMapping("/greeting/{message}")
+    public ResponseEntity<?> getGreeting(@PathVariable("message") String message){
+        Greeting greeting = new Greeting("");
+        greeting.setContent("Hello Robert"+message);
+        return new ResponseEntity<Greeting>(greeting, HttpStatus.OK);
+    }
+}
